f59e589069e8d3ba9d5d27dea5bff223d54187a1
x <x@x>
2022-11-17 10:16:57 +0100

    add pipe module for new events

---
+++ server/pipe.py
@@ -0,0 +1,36 @@
+import errno, os, posix, stat, sys
+
+def lazy_init_pipe(filename):
+    try:
+        if not stat.S_ISFIFO(os.lstat(filename).st_mode):
+            print(f"warning: there is an existing file {filename}!",
+                  file=sys.stderr)
+            return False
+    except FileNotFoundError:
+        # Create the named pipe
+        os.mkfifo(filename)
+        print(f"Created named pipe for events: {filename}")
+    return True
+
+def write_pipe(filename, data):
+    if not lazy_init_pipe(filename):
+        return
+
+    try:
+        f = posix.open(filename, posix.O_WRONLY | posix.O_NONBLOCK)
+    except OSError as ex:
+        if ex.errno == errno.ENXIO:
+            print("warning: dropping event since no subscribers to pipe")
+    else:
+        with open(f, "w") as f:
+            f.write(data)
+
+if __name__ == "__main__":
+    # Run this program once to initialize the pipe.
+    # Then in another terminal run:
+    #
+    #     $ cat /tmp/tau2
+    #
+    # and rerun this program.
+    write_pipe("/tmp/tau2", "hello")
+
