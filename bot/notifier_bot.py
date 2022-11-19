f8f07cb1e46aff5f00f9860870f2914defc96aa8
Dastan-glitch <Dastan.ManDan@gmail.com>
2022-11-19 05:35:26 +0300

    create bot cli to remove any hardcoded data

--- bot/notifier_bot.py
+++ bot/notifier_bot.py
@@ -1,3 +1,4 @@
+import argparse
 import socket
 
 class IRC:
@@ -16,20 +17,25 @@ class IRC:
         self.irc.send(bytes("NICK " + botnick + "\n", "UTF-8"))
         self.irc.send(bytes("JOIN " + channel + "\n", "UTF-8"))
 
-channel = "#dev"
-server = "127.0.0.1"
-port = 11066
-nickname = "tau-notifier"
+# parse arguments
+parser = argparse.ArgumentParser(description='IRC bot to send a pipe to an IRC channel')
+parser.add_argument('--server',default='127.0.0.1', help='IRC server')
+parser.add_argument('--port', default=11066, help='port of the IRC server')
+parser.add_argument('--nickname', help='bot nickname in IRC')
+parser.add_argument('--channel', default="#dev", action='append', help='channel to join')
+parser.add_argument('--pipe', default="/tmp/tau2" , help='pipe to read from')
+
+args = parser.parse_args()
 
 irc = IRC()
-irc.connect(server, port, channel, nickname)
+irc.connect(args.server, args.port, args.channel, args.nickname)
 
 while True:
-    with open("/tmp/tau2") as handle:
+    with open(args.pipe) as handle:
         while True:
             log_line = handle.readline()
             if not log_line:
                 break
             print(log_line)
-            print("Sending: "+log_line+" in: "+channel)
-            irc.send(channel, log_line)
+            print("Sending: "+log_line+" in: "+args.channel)
+            irc.send(args.channel, log_line)
